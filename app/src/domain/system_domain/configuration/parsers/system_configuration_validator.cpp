8fea3a4bc11a8f7e3b04ba9470c6697a307e6663
Pavel Maloletkov <pavllick@gmail.com>
2026-01-18 15:10:27 -0500

    SystemConfiguration moved to common

--- app/src/domain/system_domain/configuration/parsers/system_configuration_validator.cpp
+++
@@ -1,8 +0,0 @@
-#include "system_configuration_validator.h"
-
-namespace eerie_leap::domain::system_domain::configuration::parsers {
-
-void SystemConfigurationValidator::Validate(const SystemConfiguration& configuration) {
-}
-
-} // namespace eerie_leap::domain::system_domain::configuration::parsers
